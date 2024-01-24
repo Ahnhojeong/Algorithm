fa7586f0662ba9d3e8822890cbd1539326d9c62c
Ahnhojeong <77098852+Ahnhojeong@users.noreply.github.com>
2024-01-24 21:52:32 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,20 @@
+function solution(n, lost, reserve) {
+    let lostStudent = lost.filter(v => !reserve.includes(v)).sort();
+    let reserveStudent = reserve.filter(v => !lost.includes(v)).sort();
+    
+    for(let i=0; i<reserveStudent.length; i++) {
+        if(lostStudent.includes(reserveStudent[i] - 1)) {
+            lostStudent = lostStudent.filter((v) => {
+                    return v !== reserveStudent[i]-1;
+                });
+        } else if(lostStudent.includes(reserveStudent[i] + 1)) {
+            lostStudent = lostStudent.filter((v)=> {
+                return v !== reserveStudent[i]+1
+            });
+        }
+    }
+    
+        return n - lostStudent.length;
+}
+    
+
